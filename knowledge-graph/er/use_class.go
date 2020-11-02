f52e47948aa424407ff0bf09475e1c1df90b5294
hanzhe <sith0008@e.ntu.edu.sg>
2020-11-02 17:02:44 +0800

    use full name as in ura website

--- knowledge-graph/er/use_class.go
+++ knowledge-graph/er/use_class.go
@@ -5,51 +5,51 @@ type SpecificUseClass string
 const (
 	// Food and Beverage
 	SpecificUseClass_Restaurant    SpecificUseClass = "Restaurant"
-	SpecificUseClass_BarPub        SpecificUseClass = "BarPub"
-	SpecificUseClass_RestaurantBar SpecificUseClass = "RestaurantBar"
+	SpecificUseClass_BarPub        SpecificUseClass = "Bar/Pub"
+	SpecificUseClass_RestaurantBar SpecificUseClass = "Restaurant and Bar"
 	SpecificUseClass_Nightclub     SpecificUseClass = "Nightclub"
 
 	// Shop, Office and Services
 	SpecificUseClass_Shop                 SpecificUseClass = "Shop"
 	SpecificUseClass_Laundromat           SpecificUseClass = "Laundromat"
 	SpecificUseClass_Office               SpecificUseClass = "Office"
-	SpecificUseClass_MassageEstablishment SpecificUseClass = "MassageEstablishment"
-	SpecificUseClass_MedicalClinic        SpecificUseClass = "MedicalClinic"
-	SpecificUseClass_PetShop              SpecificUseClass = "PetShop"
-	SpecificUseClass_PetBoarding          SpecificUseClass = "PetBoarding"
+	SpecificUseClass_MassageEstablishment SpecificUseClass = "Massage Establishment"
+	SpecificUseClass_MedicalClinic        SpecificUseClass = "Medical Clinic"
+	SpecificUseClass_PetShop              SpecificUseClass = "Pet Shop"
+	SpecificUseClass_PetBoarding          SpecificUseClass = "Pet Boarding"
 
 	// Education
-	SpecificUseClass_CommercialSchool SpecificUseClass = "CommercialSchool"
-	SpecificUseClass_Childcare        SpecificUseClass = "Childcare"
+	SpecificUseClass_CommercialSchool SpecificUseClass = "Commercial School"
+	SpecificUseClass_Childcare        SpecificUseClass = "Childcare Centre"
 
 	// Sports and Recreation
-	SpecificUseClass_FitnessCentre   SpecificUseClass = "FitnessCentre"
-	SpecificUseClass_AmusementCentre SpecificUseClass = "AmusementCentre"
+	SpecificUseClass_FitnessCentre   SpecificUseClass = "Fitness Centre"
+	SpecificUseClass_AmusementCentre SpecificUseClass = "Amusement Centre"
 
 	// Accommodation
 	SpecificUseClass_Residential      SpecificUseClass = "Residential"
-	SpecificUseClass_BackpackerHostel SpecificUseClass = "BackpackerHostel"
+	SpecificUseClass_BackpackerHostel SpecificUseClass = "Backpackers' Hostel"
 	SpecificUseClass_Hotel            SpecificUseClass = "Hotel"
-	SpecificUseClass_StudentHostel    SpecificUseClass = "StudentHostel"
-	SpecificUseClass_ServiceApartment SpecificUseClass = "ServiceApartment"
-	SpecificUseClass_WorkerDorm       SpecificUseClass = "WorkerDorm"
+	SpecificUseClass_StudentHostel    SpecificUseClass = "Students' Hostel"
+	SpecificUseClass_ServiceApartment SpecificUseClass = "Serviced Apartment"
+	SpecificUseClass_WorkerDorm       SpecificUseClass = "Workers' Dormitories"
 
 	// Industrial-related uses
-	SpecificUseClass_LightIndUse         SpecificUseClass = "LightIndUse"
-	SpecificUseClass_GeneralIndUse       SpecificUseClass = "GeneralIndUse"
-	SpecificUseClass_IndTraining         SpecificUseClass = "IndTraining"
+	SpecificUseClass_LightIndUse         SpecificUseClass = "Light Industrial Use"
+	SpecificUseClass_GeneralIndUse       SpecificUseClass = "General Industrial Use"
+	SpecificUseClass_IndTraining         SpecificUseClass = "Industrial Training"
 	SpecificUseClass_Warehouse           SpecificUseClass = "Warehouse"
-	SpecificUseClass_IndCanteen          SpecificUseClass = "IndCanteen"
+	SpecificUseClass_IndCanteen          SpecificUseClass = "Industrial Canteen"
 	SpecificUseClass_Showroom            SpecificUseClass = "Showroom"
-	SpecificUseClass_EBusiness           SpecificUseClass = "EBusiness"
-	SpecificUseClass_CoreMediaActivities SpecificUseClass = "CoreMediaActivities"
+	SpecificUseClass_EBusiness           SpecificUseClass = "E-Business"
+	SpecificUseClass_CoreMediaActivities SpecificUseClass = "Core Media Activities"
 
 	// Community-related uses
-	SpecificUseClass_CommunityClub SpecificUseClass = "CommunityClub"
+	SpecificUseClass_CommunityClub SpecificUseClass = "Association/CommunityClub/Family Service Centre"
 
 	// Religious Use
-	SpecificUseClass_ReligiousActivities SpecificUseClass = "ReligiousActivities"
-	SpecificUseClass_LimitedReligiousUse SpecificUseClass = "LimitedReligiousUse"
+	SpecificUseClass_ReligiousActivities SpecificUseClass = "Religious Activities"
+	SpecificUseClass_LimitedReligiousUse SpecificUseClass = "Limited & Non-Exclusive Religious Use"
 )
 
 type GenericUseClass string
