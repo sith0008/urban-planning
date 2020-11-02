fb9266f775d1ed6742902ef9611b7731e99a0fd5
hanzhe <sith0008@e.ntu.edu.sg>
2020-11-02 17:03:09 +0800

    init db with sample cases

--- knowledge-graph/init/main.go
+++ knowledge-graph/init/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/neo4j/neo4j-go-driver/neo4j"
 	. "github.com/sith0008/urban-planning/knowledge-graph/er"
@@ -14,6 +17,28 @@ const (
 	DB_Username = "neo4j"
 	DB_Password = "password"
 )
+
+type ProcessedCaseLocation struct {
+	Case     ProcessedCase     `json: case`
+	Location ProcessedLocation `json: location`
+}
+
+type ProcessedCase struct {
+	Id               string           `json: id`
+	ProposedUseClass SpecificUseClass `json: proposedUseClass`
+	ProposedUseDesc  string           `json: proposedUseDesc`
+	GFA              float64          `json: gfa`
+	Decision         string           `json: decision`
+	Evaluation       string           `json: evaluation`
+}
+
+type ProcessedLocation struct {
+	PostalCode   float64          `json: postalCode`
+	Lot          string           `json: lot`
+	Floor        int64            `json: floor`
+	Unit         int64            `json: unit`
+	PropertyType SpecificPropType `json: propertyType`
+}
 
 func main() {
 	_, dbSession, err := InitialiseDBSession(DB_Host, DB_Port, DB_Username, DB_Password)
@@ -29,6 +54,126 @@ func main() {
 	if err != nil {
 		// TODO: handle error
 	}
+	var cases []ProcessedCaseLocation
+	jsonFile, err := os.Open("processed-samples.json")
+	defer jsonFile.Close()
+	casesJson, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal([]byte(casesJson), &cases)
+	err = InitialiseSampleData(dbSession, cases)
+}
+
+func InitialiseSampleData(dbSession neo4j.Session, cases []ProcessedCaseLocation) error {
+	tx, err := dbSession.BeginTransaction()
+	if err != nil {
+		log.Println("[ERROR]: failed to start transaction")
+		log.Fatal(err)
+		return err
+	}
+	for _, c := range cases {
+		// insert case
+		insertCaseResult, err := tx.Run("CREATE (c:Case) SET c.proposed_use = $proposedUse, c.GFA = $GFA, c.decision = $decision, c.evaluation = $evaluation RETURN id(c)",
+			map[string]interface{}{
+				"proposedUse": c.Case.ProposedUseDesc,
+				"GFA":         c.Case.GFA,
+				"decision":    c.Case.Decision,
+				"evaluation":  c.Case.Evaluation,
+			})
+		if err != nil {
+			log.Println("[ERROR]: failed to insert case")
+			log.Println(err)
+			return err
+		}
+		var caseId int64
+		if insertCaseResult.Next() {
+			caseId = insertCaseResult.Record().GetByIndex(0).(int64)
+			log.Printf("[INFO] Inserted case: %d", caseId)
+		}
+		// insert case-uc relation
+		insertCaseUseClassRelationResult, err := tx.Run("MATCH (c:Case), (s:SpecificUseClass) WHERE id(c) = $caseId AND s.name = $proposedUseClass CREATE (c)-[r:HAS_USE_CLASS]->(s) RETURN id(r)",
+			map[string]interface{}{
+				"caseId":           caseId,
+				"proposedUseClass": c.Case.ProposedUseClass,
+			})
+		if err != nil {
+			log.Println("[ERROR]: failed to insert HAS_USE_CLASS relation")
+			log.Println(err)
+			return err
+		}
+		var caseUseClassRelationId int64
+		if insertCaseResult.Next() {
+			caseUseClassRelationId = insertCaseUseClassRelationResult.Record().GetByIndex(0).(int64)
+			log.Printf("[INFO] Inserted case-uc relation: %d", caseUseClassRelationId)
+		}
+		// insert location
+		var locationId int64
+		getLocationResult, err := tx.Run("MATCH (l:location) WHERE l.postalCode = $postalCode AND l.floor = $floor AND l.unit = $unit RETURN id(l)",
+			map[string]interface{}{
+				"postalCode": c.Location.PostalCode,
+				"floor":      c.Location.Floor,
+				"unit":       c.Location.Unit,
+			})
+		if getLocationResult.Next() {
+			locationId = getLocationResult.Record().GetByIndex(0).(int64)
+			log.Printf("[INFO] Retrieved location: %d", locationId)
+		} else {
+			insertLocationResult, err := tx.Run("CREATE (l:Location) SET l.postalCode = $postalCode, l.floor = $floor, l.unit = $unit RETURN id(l)",
+				map[string]interface{}{
+					"postalCode": c.Location.PostalCode,
+					"floor":      c.Location.Floor,
+					"unit":       c.Location.Unit,
+				})
+			if err != nil {
+				log.Println("[ERROR]: failed to insert location")
+				log.Println(err)
+				return err
+			}
+			if insertLocationResult.Next() {
+				locationId = insertLocationResult.Record().GetByIndex(0).(int64)
+				log.Printf("[INFO] Inserted location: %d", locationId)
+			}
+		}
+		// insert location-proptype relation
+		insertLocationPropTypeRelationResult, err := tx.Run("MATCH (l:Location), (s:SpecificPropType) WHERE id(l) = $locationId AND s.name = $propType CREATE (l)-[r:HAS_PROP_TYPE]->(s) RETURN id(r)",
+			map[string]interface{}{
+				"locationId": locationId,
+				"propType":   c.Location.PropertyType,
+			})
+		if err != nil {
+			log.Println("[ERROR]: failed to insert HAS_PROP_TYPE relation")
+			log.Println(err)
+			return err
+		}
+		var locationPropTypeRelationId int64
+		if insertCaseResult.Next() {
+			locationPropTypeRelationId = insertLocationPropTypeRelationResult.Record().GetByIndex(0).(int64)
+			log.Printf("[INFO] Inserted location-pt relation: %d", locationPropTypeRelationId)
+		}
+		// insert case-location relation
+		insertRelationResult, err := tx.Run("MATCH (c:Case),(l:Location) WHERE id(c) = $caseId AND id(l) = $locationId CREATE (c)-[r:LOCATED_IN]->(l) RETURN id(r)",
+			map[string]interface{}{
+				"caseId":     caseId,
+				"locationId": locationId,
+			})
+		if err != nil {
+			log.Println("[ERROR]: failed to insert LOCATED_IN relation")
+			log.Println(err)
+			return err
+		}
+		var relationId int64
+		if insertRelationResult.Next() {
+			relationId = insertRelationResult.Record().GetByIndex(0).(int64)
+			log.Printf("[INFO] Inserted LOCATED_IN relation: %d", relationId)
+
+		} else {
+			log.Println("[WARN] Cannot find match")
+			tx.Rollback()
+			return err
+		}
+
+	}
+
+	tx.Commit()
+	return nil
 }
 
 func InitialiseUseClass(dbSession neo4j.Session, useClassMap map[SpecificUseClass]GenericUseClass) error {
